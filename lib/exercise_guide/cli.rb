1804031e76e53c3c2cebcd59067171f43b7d6043
Indre Bakeer <indre.bakeer@gmail.com>
2019-05-15 18:38:51 +0000

    cli notes

--- lib/exercise_guide/cli.rb
+++ lib/exercise_guide/cli.rb
@@ -2,5 +2,11 @@ class ExerciseGuide::CLI
 
   def start
     puts "Welcome to the Exercise Guide App!"
+    puts "Please type a body part that you would like to exercise"
+    #list body parts
+    #gets input
+    #lists all exercises for that body part
+    puts "Type the exercise for more details"
+    #puts description and link for video
   end
 end
