7e58d43e03ea3b835fa7bc5241e88553451181c5
Indre Bakeer <indre.bakeer@gmail.com>
2019-05-20 20:07:54 +0000

    added notes and attr accessor to turn into objects

--- lib/exercise_guide/exercise.rb
+++ lib/exercise_guide/exercise.rb
@@ -1,2 +1,8 @@
 class ExerciseGuide::Exercise
+  attr_accessor :muscle_name, :muscle_url
+
+  def initialize(list)
+
+  end
+
 end
--- lib/exercise_guide/scraper.rb
+++ lib/exercise_guide/scraper.rb
@@ -2,8 +2,10 @@ class ExerciseGuide::Scraper
 
     def self.scrape_body_parts
       doc = Nokogiri::HTML(open("https://www.bodybuilding.com/exercises/"))
+
+      array_body_parts = doc.css("div.exercise-list li")
       list = []
-      doc.css("div.exercise-list li").each do |muscle|
+      array_body_parts.each do |muscle|
         scraped_hash = {
           muscle_name: muscle.css("a").children.text,
           muscle_url: muscle.css("a").attribute("href").value
@@ -11,13 +13,26 @@ class ExerciseGuide::Scraper
         list << scraped_hash
       end
       list
+      binding.pry
     end
 
-    def self.scrape_exercises
-      #doc.css("dicv.ExCategory-results")
+    def self.scrape_exercise_list
+      # shows the list of results based on the muscle selected with equipment type
+
+      # .css("div.ExCategory-results") <== exercise list titles
+      # .css("a").attribute("href").value <== link to exercise
+    end
+
+    def self.scrape_exercise
+      # shows exercise selected and includes description
+
+      # .ccs(".ExHeading ExHeading--h3") <== title of exercise
+      # .css(".bb-list--plain").text <== shows equipment type, either "body only" or "machine"
+      # .css("div.grid-8 grid-12-s grid-12-m").text <== Instructions
+      # .css("div.jw-media jw-reset").value || .css("video.jw-media jw-reset").value <== video example or include pics
     end
 
 end
 
-# ExerciseGuide::Scraper.scrape_body_parts
+ ExerciseGuide::Scraper.scrape_body_parts
 # ExerciseGuide::Scraper.scrape_exercises
