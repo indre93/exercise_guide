84ecd6fafd4234292f1bfc9e46000ebf6bf4065d
Indre Bakeer <indre.bakeer@gmail.com>
2019-05-20 20:48:46 +0000

    additional methods w notes

--- lib/exercise_guide/cli.rb
+++ lib/exercise_guide/cli.rb
@@ -7,11 +7,19 @@ class ExerciseGuide::CLI
     puts ExerciseGuide::Scraper.scrape_body_parts
     #Make sure to only list muscle names - not muscle url
     puts "            -----------------"
-
-    #list body parts
     #gets input
-    #lists all exercises for that body part
+
     #puts "Type the exercise for more details"
     #puts description and link for video
   end
+
+  def list_body_parts
+    #list body parts that would like to exercise
+  end
+
+  def list_exercises
+    #lists all exercises for that body part
+  end
+
+
 end
