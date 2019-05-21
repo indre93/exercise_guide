227777a34070474633292ad8892593085e8d8ae1
Indre Bakeer <indre.bakeer@gmail.com>
2019-05-21 19:09:21 +0000

    updated app design

--- lib/exercise_guide/cli.rb
+++ lib/exercise_guide/cli.rb
@@ -2,31 +2,31 @@ class ExerciseGuide::CLI
 
   def start
     puts ""
-    puts "            -----------------"
-    puts "            -----------------"
+    puts "     ----------------------------------"
     puts "     Welcome to the Exercise Guide App!"
-    puts "            -----------------"
-    puts "            -----------------"
+    puts "     ----------------------------------"
+    puts ""
     ExerciseGuide::Scraper.scrape_body_parts
     list_body_parts
     puts ""
-    puts "Type the number of the muscle that you would like to exercise:"
+    puts " Type the number of the muscle that you would like to exercise:"
     #gets input and lists exercises based on input
     puts ""
     puts "           ------------------"
     puts "            Exercise results"
     puts "           ------------------"
     puts ""
+    puts "    ========================================="
     ExerciseGuide::Scraper.scrape_exercises
     list_exercises
     puts ""
-    puts "Type the number of the exercise for more details:"
+    puts " Type the number of the exercise for more details:"
     #gets input and shows exercise instructions based in input
     puts ""
-    puts "            ----------------"
-    ExerciseGuide::Scraper.scrape_instructions
+    puts "             --------------"
     puts "              Instructions"
-    puts "            ----------------"
+    puts "             --------------"
+    ExerciseGuide::Scraper.scrape_instructions
     exercise_instructions
     puts ""
     puts "            -----------------"
@@ -34,22 +34,22 @@ class ExerciseGuide::CLI
 
   def list_body_parts
     ExerciseGuide::BodyPart.all.each.with_index(1) do |body_part, index, rating|
-      puts "#{index}. #{body_part.name}"
+      puts " #{index}. #{body_part.name}"
     end
   end
 
   def body_part_url
     ExerciseGuide::BodyPart.all.each do |body_part|
-      puts "#{body_part.body_part_link}"
+      puts " #{body_part.body_part_link}"
     end
   end
 
   def list_exercises
     ExerciseGuide::Exercise.all.each.with_index(1) do |exercise, index|
-      puts "#{index}. #{exercise.exercise_title}"
-      puts "   Rating: #{exercise.rating}"
-      puts "   #{exercise.equipment_type}"
-      puts ""
+      puts " #{index}. #{exercise.exercise_title}"
+      puts "    Rating: #{exercise.rating}"
+      puts "    #{exercise.equipment_type}"
+      puts "    ========================================="
     end
   end
 
