b5f05404fb80936a6d0cacc4945c93fc3da80c0d
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-21 20:39:14 +0000

    update

--- lib/exercise_guide/exercise.rb
+++ lib/exercise_guide/exercise.rb
@@ -1,6 +1,5 @@
 class ExerciseGuide::Exercise
-  attr_accessor :exercise_title, :equipment_type, :exercise_rating, :exercises_link, :muscle
-
+  attr_accessor :exercise_title, :exercise_rating, :exercises_link, :muscle
   @@all = []
 
   # turns all attributes into objects
